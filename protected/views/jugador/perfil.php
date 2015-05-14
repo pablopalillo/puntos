1eb59cd3ec5afc7c68e295ee2e5f456fccac3966
pablopalillo <pablo.576@hotmail.com>
2015-05-14 17:33:36 -0500

    estructura perfil

--- protected/views/jugador/perfil.php
+++ protected/views/jugador/perfil.php
@@ -3,17 +3,31 @@
 ?>
 
 <div id="estadisticas" class="col-sm-4">
+	<header>
+		<?php echo CHtml::link( 'PARTICIPA', array('/jugar'), array('class' => 'btn btn-primary btn-lg btn-block' ) ); ?>
+		<h1>Estadísticas</h1>
+	</header>
+	<section class="col-sm-12" id="estadisticas-content">
 
-	<?php //echo CHtml::link( '¡Estamos verificando <span>los resultados!</span>', array('/puntajes'), array('class' => 'juega' ) ); ?>
-	<header></header>
-	<section id="estadisticas-content">
-		<h3>Estadísticas</h3>
+		<div class="col-sm-11">
+			<h3> Posicion actual</h3>
+			<span> 15 </span>
+		</div>
 
-		<p> Puntaje del mes actual <span> 16 </span> </p>
-		<p> Puntaje acomulado del año <span> 1500 </span> </p>
-		<p> Posicion actual <span> 15 </span> </p>
+		<div class="col-sm-11">
+			<h3> Puntaje del mes actual  </h3>
+			<span> 16 </span>
+		</div>
+		<div class="col-sm-11">
+			<h3>Puntaje acomulado del año</h3>
+			<span> 1500 </span>
+		</div>
 
-		<p> Puntajetotal <span><?php echo $jugador->puntaje ?> </span></p>
+		<div class="col-sm-11">
+			<h3> Puntaje total</h3>
+			<span> <?php echo $jugador->puntaje ?> </span>
+		</div>
+
 	<!-- 	<p> Rondas jugadas <span> <?php //echo $estadisticas['rondas'] ?> </span></p> -->
 	<!--	<p>Tiempo total de juego <span> <?php //echo $estadisticas['tiempo'] ?> </span></p> -->
 <!--		<p>Total de preguntas resueltas <span> <?php //echo $estadisticas['preguntas'] ?> </span></p> -->
@@ -36,26 +50,30 @@
 
 	<section id="perfil-content">
 		<header>
-			<h2>Datos de acceso</h2>
+			<strong>Datos de acceso</strong>
 			<p>Correo: <?php echo $jugador->usuario->correo ?> </p>
 			<p>Nombre: <?php echo $jugador->nombre_adulto ?></p>
 		</header>
 
-		<h2>Datos personales</h2>
+		<strong>Datos personales</strong>
 
 		<p>Nombre: <?php echo $jugador->nombre ?> </p>
 		<p>Edad : <?php echo $jugador->getEdad() ?> </p>
-		<p>Tarjeta de identidad número: <?php echo $jugador->documento ?></p>
+		<p>Documento Identidad: <?php echo $jugador->documento ?></p>
 		<p>Telefono fijo: <?php echo $jugador->telefono ?> </p>
 
 		<?php if( ! empty($jugador->correo_adulto) ): ?>
-			<h2>Información del adulto responsable</h2>
+			<strong>Adulto responsable </strong>
 
 			<p>Correo: <?php echo $jugador->correo_adulto ?></p>
 			<p>Documento: <?php echo $jugador->documento_adulto ?></p>
+		<?php endif; ?>
 
-	<?php endif; ?>
-
+		<div >
+			<?php echo CHtml::link( 'Editar Información', Yii::app()->request->baseUrl . '/participar', array('class'=>'btn btn-primary') ); ?>
+		</div>
 	</section>
+
+
 
 </div>
