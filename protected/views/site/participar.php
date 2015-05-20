3d11961f3dd63a14ad06273ad9ffc87ccfb649f6
emergugue <emergugue@gmail.com>
2015-05-20 17:57:12 -0500

    Se dio estilo a la página de loguin

--- protected/views/site/participar.php
+++ protected/views/site/participar.php
@@ -7,9 +7,10 @@ $this->pageTitle= 'Participar - ' .Yii::app()->name;
 
 ?>
 
-<div class="form col-sm-6">
-  <div class="well">
+<div class="form col-sm-6 ctn_iniciar-sesion">
+  <div class="iniciar-sesion_title">
     <h1>Iniciar sesión</h1>
+    <p>Si ya estás registrado, inicia sesión en esta opción</p>
   </div>
 
   <?php $form=$this->beginWidget('CActiveForm', array(
@@ -22,42 +23,45 @@ $this->pageTitle= 'Participar - ' .Yii::app()->name;
   )); ?>
 
 	<?php echo $form->errorSummary($model, '', '', array('class' => 'flash-notice') ); ?>
+  <div class="ctn_formulario">
+  	<div class="form-group">
+  		<?php echo $form->label($model,'correo'); ?>
+  		<?php echo $form->emailField($model,'correo', array('class' => 'form-control')); ?>
+  	</div>
 
-	<div class="form-group">
-		<?php echo $form->label($model,'correo'); ?>
-		<?php echo $form->emailField($model,'correo', array('class' => 'form-control')); ?>
-	</div>
+  	<div class="form-group">
+  		<?php echo $form->label($model,'password'); ?>
+  		<?php echo $form->passwordField($model,'password', array('class' => 'form-control')); ?>
+  	</div>
 
-	<div class="form-group">
-		<?php echo $form->label($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password', array('class' => 'form-control')); ?>
-	</div>
+  	<div class="form-group rememberMe">
 
-	<div class="form-group rememberMe">
+      <div class="checkbox-inline">
+        <label>
+          <?php echo $form->checkBox($model,'rememberMe'); ?>
+          <?php echo $form->label($model,'rememberMe'); ?>
+        </label>
+      </div>
+  	</div>
 
-    <div class="checkbox-inline">
-      <label>
-        <?php echo $form->checkBox($model,'rememberMe'); ?>
-        <?php echo $form->label($model,'rememberMe'); ?>
-      </label>
-    </div>
-	</div>
+  	<div class="form-group buttons">
+  		<?php echo CHtml::submitButton('Iniciar sesión', array('class' =>'btn btn-default btn-general_lg')); ?>
+  	</div>
 
-	<div class="form-group buttons">
-		<?php echo CHtml::submitButton('Iniciar sesión', array('class' =>'btn btn-default ')); ?>
-	</div>
-
-	<div class="form-group">
-		<?php echo CHtml::link('¿Olvidaste la contraseña?', array('/recuperar-contrasena'), array('class' => 'recuperar' )); ?>
-	</div>
-
+  	<div class="form-group ctn_recuperar">
+  		<?php echo CHtml::link('¿Olvidaste la contraseña?', array('/recuperar-contrasena'), array('class' => 'recuperar' )); ?>
+  	</div>
+  </div>
 <?php $this->endWidget(); ?>
 </div><!-- form -->
 
 <div class="col-sm-6">
-  <div class="well">
-    <span>Si es la primera vez que vas a participar, debes registrarte </span>
-  </div>
+  <div class="ctn_registro">
+    <div class="iniciar-sesion_title">
+      <h1>Registrarse</h1>
+      <p>Si es la primera vez que vas a participar, debes registrarte </p>
+    </div>
 
-	<?php echo CHtml::link('Registrarme', array('registro'), array('class' => 'btn btn-primary btn-lg btn-block') )?>
+  	<?php echo CHtml::link('Registrarme', array('registro'), array('class' => 'btn btn-primary btn-lg btn-general_lg') )?>
+  </div>
 </div>
