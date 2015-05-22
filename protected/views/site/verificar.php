2159ae7f12bcde5f029f51a72c19709acbd9a2fc
emergugue <emergugue@gmail.com>
2015-05-22 18:24:37 -0500

    Se dio estilo a las páginas de confirmación del registro

--- protected/views/site/verificar.php
+++ protected/views/site/verificar.php
@@ -1,5 +1,5 @@
 <div id="content" class="row">
-  <div class="col-sm-6">
+  <div class="ctn_contenido verificar-correo">
 		<?php if($mensaje == 'correcto'): ?>
 			<h2>¡Listo!</h2>
 			<p>Nos hemos dado cuenta que ya estás listo para empezar a jugar y poder ganar el premio de un viaje a Suiza para comprobar que Medellín puede ser la sede de los Juegos Olímpicos de la Juventud 2018</p>
--- protected/views/site/verificar_correo.php
+++ protected/views/site/verificar_correo.php
@@ -1,7 +1,7 @@
 
 <div id="content" class="row">
-  <div class="col-sm-6">
-    <?php echo '<p>Hola ' . strtok($datos['nombre'], ' ') . '</p>'; ?>
+  <div class="ctn_contenido verificar-correo">
+    <h2><?php echo '<p>Hola ' . strtok($datos['nombre'], ' ') . '</p>'; ?></h2>
     <p>Estás cerca iniciar el juego para participar por el viaje a Suiza gracias los Juegos Olímpicos de la Juventud Medellín 2018. Revisa tu correo electrónico para poder continuar.</p>
     <p>No olvides que al correo electrónico del adulto que inscribiste también llegará un mensaje.</p>
   </div>
